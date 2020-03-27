64f024fa9eb24b0fd72f953afb6b9993a304be70
Huriel Lopes <huriellopes1996@gmail.com>
2020-03-27 14:37:23 -0300

    Testes, validations e deploy heroku

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
+
+// Controllers
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
@@ -6,15 +9,41 @@ const SessionsController = require('./controllers/SessionController')
 
 const routes = express.Router()
 
-routes.post('/sessions', SessionsController.create)
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required(),
+    })
+}), SessionsController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
 
-routes.get('/profile', ProfileController.index)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create)
 
-routes.get('/incidents', IncidentController.index)
+routes.get('/profile', celebrate({
+    [Segments.BODY]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown()
+}), ProfileController.index)
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.index)
 routes.post('/incidents', IncidentController.create)
-routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete)
 
 module.exports = routes
