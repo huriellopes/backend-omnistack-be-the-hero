2d31c5d9ab61bfd3a8a2b4b13d9bb3d8b034ae4f
Huriel Lopes <huriellopes1996@gmail.com>
2020-03-27 23:52:03 -0300

    Tentativa

--- src/app.js
+++ src/app.js
@@ -6,9 +6,6 @@ const routes = require('./routes')
 const app = express()
 
 app.use(cors()) // prod: origin: http://meuapp.com
-app.use(function(req, res, next) {
-    res.header('Access-Control-Expose-Headers', 'X-Total-Count')
-})
 app.use(express.json())
 app.use(routes)
 app.use(errors())
