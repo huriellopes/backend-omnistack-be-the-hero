f32bb96f3050cac84a14dda3639ddd63dc0a2a92
Huriel Lopes <huriellopes1996@gmail.com>
2020-03-27 23:09:00 -0300

    Tentativa de correção

--- src/app.js
+++ src/app.js
@@ -6,8 +6,11 @@ const routes = require('./routes')
 const app = express()
 
 app.use(cors()) // prod: origin: http://meuapp.com
-app.use(function (req, res, next) {
-    res.set('X-XSS-Protection', 0);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 })
 app.use(express.json())
